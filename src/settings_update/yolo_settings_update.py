91174f8ed2cccaec20243596ffe75504ed82cd21
Aleksandr Sukharev <iebebd3@gmail.com>
2025-01-22 19:09:26 +0300

    feat: [#3] datasets_dir

--- src/settings_update/yolo_settings_update.py
+++ src/settings_update/yolo_settings_update.py
@@ -1,8 +1,7 @@
-from pathlib import Path
-
 from src.constantsconfigs.config import YoloSettingsUpdate
 from ultralytics import settings
 from src.constantsconfigs.constants import PROJECT_DEFAULT_PATH
+from src.utils.load_config import load_config
 
 
 def settings_update(cfg: YoloSettingsUpdate):
@@ -13,6 +12,15 @@ def settings_update(cfg: YoloSettingsUpdate):
         {
             "clearml": cfg.clearml,
             "mlflow": cfg.mlflow,
-            "datasets_dir": PROJECT_DEFAULT_PATH,
+            "datasets_dir": str(cfg.data_path),
         },
     )
+
+
+def main():
+    config_path = "configs/trainerconfigs/trainer_config_cars.yaml"
+    config = load_config(PROJECT_DEFAULT_PATH / config_path)
+    settings_update(config.yolo_settings_update)
+
+if __name__ == '__main__':
+    main()
